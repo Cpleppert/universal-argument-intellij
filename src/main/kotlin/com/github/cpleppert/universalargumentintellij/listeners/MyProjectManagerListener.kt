8eb2724f749f3f66877b9c3f0649ecfacdeb5ee1
GitHub Action <action@github.com>
2021-08-03 01:19:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/cpleppert/universalargumentintellij/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/cpleppert/universalargumentintellij/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cpleppert.universalargumentintellij.listeners
 
+import com.github.cpleppert.universalargumentintellij.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/cpleppert/universalargumentintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cpleppert.universalargumentintellij.services
+
+import com.github.cpleppert.universalargumentintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
